dfd90dcfe1ef2842270e2f65190b4342f60c99ad
taro1025 <game4967@gmail.com>
2021-11-03 22:40:56 +0900

    feat:普通に持ってるメモと過去のメモを切り替えられるように

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,78 @@
 import {NextPage} from 'next'
-import { MemoCard } from '../components/MemoCard'
+import {MemoCard} from '../components/MemoCard'
 import Link from "next/link"
+import {useState} from "react"
+
+interface MemoInterface {
+    title: string;
+    text: string;
+    created_at: string;
+    updated_at: string;
+}
+
+interface IndexMemosInterface {
+    currentMemos: MemoInterface[]; //今日書いたメモ+永久化してるメモ
+    pastMemos: MemoInterface[];
+}
 
 const Index: NextPage = () => {
 
     // going to fetch data
     // {
     //     permanentMemos: {
-    //         dfjdsfldf
+    //         memo: {
+    //             title: 'fdlfd'
+    //             text: 'fdjflds'
+    //          }
     //     }
     //     currentMemos: {
-    //         dfdfdfdfdf
-    //     }
+    //         memo: {
+    //          title: 'fdlfd'
+    //          text: 'fdjflds'
+    //          }    //     }
     //     pastMemos: {
-    //         kldfjdfdsfs
-    //     }
+    //         memo: {
+    //          title: 'fdlfd'
+    //          text: 'fdjflds'
+    //         }    //     }
     // }
+
+    const [memoIndex, setMemoIndex] = useState<IndexMemosInterface>()
+    const [isCurrentMemoIndex, switchMemoIndex] = useState<boolean>(true)
+
+    const handleSwitch = () => {
+        switchMemoIndex(!isCurrentMemoIndex)
+    }
+
     return (
         <>
-            <h2>メモ一覧 or 過去３０日間のメモ</h2>
+            {
+                isCurrentMemoIndex ? (
+                    <>
+                        <h2 onClick={handleSwitch}>メモ一覧</h2>
 
-            <Link href="/memos/1">
-                <a><MemoCard
-                    title="title 今日の反省"
-                    date="日付 2012/12/12"
-                    leadSentence="read 今日は..."
-                /></a>
-            </Link>
+                        <Link href="/memos/1">
+                            <a><MemoCard
+                                title="title 今日の反省"
+                                date="日付 2012/12/12"
+                                leadSentence="read 今日は..."
+                            /></a>
+                        </Link>
+                    </>
+                ) : (
+                    <>
+                        <h2 onClick={handleSwitch}>過去３０日間のメモ</h2>
 
-            <MemoCard
-                title="title 将来の夢"
-                date="日付 2012/12/12"
-                leadSentence="read 俺は..."
-            />
+                        <Link href="/memos/1">
+                            <a> <MemoCard
+                                title="title 将来の夢"
+                                date="日付 2012/12/12"
+                                leadSentence="read 俺は..."
+                            /></a>
+                        </Link>
+                    </>
+                )
+            }
         </>
     )
 }
