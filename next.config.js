143d8c9b67b35bdaa72214a08a20e6b531ebe0a6
taro1025 <game4967@gmail.com>
2021-11-05 01:26:48 +0900

    feat:PWAに対応した

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+// /next.config.js
+//** @type {import('next/dist/next-server/server/config-shared').NextConfig} */
+
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const config = {
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/]
+  },
+};
+
+module.exports = withPWA(config);
---
+++ src/pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+    DocumentInitialProps,
+} from "next/document";
+
+class MyDocument extends Document {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
+        return await Document.getInitialProps(ctx);
+    }
+
+    render() {
+        return (
+            <Html lang="ja-JP" dir="ltr">
+                <Head>
+                    <meta name="theme-color" content="#f9a8d4" />
+                    <link rel="manifest" href="/manifest.json" />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
